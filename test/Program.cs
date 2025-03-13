11dd345118ad854d72ddb8907ea78b41dab194d2
Suleyman Sardogan <sardogansuleyman04@gmail.com>
2025-03-13 11:33:38 +0300

    First Commit

---
+++ test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Program p = new Program();
+            {
+                Console.WriteLine("Hello World");
+            }
+        }
+    }
+}
